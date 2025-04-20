841459d8fadfd8a2796b92da3be83c9235cd97cf
h4sanzade <h4sanzade@gmail.com>
2025-04-20 20:50:18 +0400

    Initial commit

---
+++ app/src/main/java/com/materialdesign/jv/LoginUI.kt
@@ -0,0 +1,56 @@
+package com.materialdesign.jv
+
+import android.graphics.Color
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.materialdesign.jv.databinding.ActivityLoginUiBinding
+
+class LoginUI : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginUiBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginUiBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        val fullText = "Artıq hesabın var? Daxil ol"
+        val spannable = SpannableString(fullText)
+
+        val redColor = ContextCompat.getColor(this, android.R.color.holo_red_dark)
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Toast.makeText(this@LoginUI, "Daxil ol clicked!", Toast.LENGTH_SHORT).show()
+
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = redColor
+                ds.isUnderlineText = false
+            }
+        }
+
+
+        val startIndex = fullText.indexOf("Daxil ol")
+        val endIndex = startIndex + "Daxil ol".length
+
+        spannable.setSpan(clickableSpan, startIndex, endIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.alreadyAcc.text = spannable
+        binding.alreadyAcc.movementMethod = LinkMovementMethod.getInstance()
+        binding.alreadyAcc.highlightColor = Color.TRANSPARENT
+    }
+}
